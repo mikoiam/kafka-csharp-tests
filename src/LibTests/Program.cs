7fd061f68299b892e6bc822808e53f77d07f27a6
Michal Kozlowski <mail@michalkozlowski.name>
2018-07-18 08:41:28 +0200

    ssl server and writer

--- src/LibTests/Program.cs
+++ src/LibTests/Program.cs
@@ -1,12 +1,120 @@
-﻿using System;
+﻿// Copyright 2016-2017 Confluent Inc., 2015-2016 Andreas Heider
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Derived from: rdkafka-dotnet, licensed under the 2-clause BSD License.
+//
+// Refer to LICENSE for more information.
 
-namespace LibTests
+using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+
+
+namespace Confluent.Kafka.Examples.Producer
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: .. brokerList topicName");
+                return;
+            }
+
+            string brokerList = args[0];
+            string topicName = args[1];
+
+            var config = new Dictionary<string, object> {
+                { "bootstrap.servers", brokerList } ,
+                { "security.protocol", "ssl" },
+                { "ssl.ca.location", @"C:\Kafka\secrets\snakeoil-ca-1.crt" },
+                { "ssl.certificate.location", @"C:\Kafka\secrets\MyClient1.crt" },
+                { "ssl.key.location", @"C:\Kafka\secrets\MyClient1.key" },
+             //   { "ssl.key.password", "ssl" },
+            };
+
+            using (var producer = new Producer<string, string>(config, new StringSerializer(Encoding.UTF8), new StringSerializer(Encoding.UTF8)))
+            {
+                Console.WriteLine("\n-----------------------------------------------------------------------");
+                Console.WriteLine($"Producer {producer.Name} producing on topic {topicName}.");
+                Console.WriteLine("-----------------------------------------------------------------------");
+                Console.WriteLine("To create a kafka message with UTF-8 encoded key and value:");
+                Console.WriteLine("> key value<Enter>");
+                Console.WriteLine("To create a kafka message with a null key and UTF-8 encoded value:");
+                Console.WriteLine("> value<enter>");
+                Console.WriteLine("Ctrl-C to quit.\n");
+
+                var cancelled = false;
+                Console.CancelKeyPress += (_, e) =>
+                {
+                    e.Cancel = true; // prevent the process from terminating.
+                    cancelled = true;
+                };
+
+                while (!cancelled)
+                {
+                    Console.Write("> ");
+
+                    string text;
+                    try
+                    {
+                        text = Console.ReadLine();
+                    }
+                    catch (IOException)
+                    {
+                        // IO exception is thrown when ConsoleCancelEventArgs.Cancel == true.
+                        break;
+                    }
+                    if (text == null)
+                    {
+                        // Console returned null before 
+                        // the CancelKeyPress was treated
+                        break;
+                    }
+
+                    string key = null;
+                    string val = text;
+
+                    // split line if both key and value specified.
+                    int index = text.IndexOf(" ");
+                    if (index != -1)
+                    {
+                        key = text.Substring(0, index);
+                        val = text.Substring(index + 1);
+                    }
+
+                    // Calling .Result on the asynchronous produce request below causes it to
+                    // block until it completes. Generally, you should avoid producing
+                    // synchronously because this has a huge impact on throughput. For this
+                    // interactive console example though, it's what we want.
+                    var deliveryReport = producer.ProduceAsync(topicName, key, val).Result;
+                    Console.WriteLine(
+                        deliveryReport.Error.Code == ErrorCode.NoError
+                            ? $"delivered to: {deliveryReport.TopicPartitionOffset}"
+                            : $"failed to deliver message: {deliveryReport.Error.Reason}"
+                    );
+                }
+
+                // Since we are producing synchronously, at this point there will be no messages
+                // in flight and no delivery reports waiting to be acknowledged, so there is no
+                // need to call producer.Flush before disposing the producer, as you typically 
+                // would.
+            }
         }
     }
 }
